0083395092ff7d6781219953569ba050a90ed0e6
gary.mariglia <gary.marigliano@he-arc.ch>
2015-04-22 17:21:10 +0200

    add regex expression in Document

--- Document.py
+++ Document.py
@@ -1,3 +1,5 @@
+import re
+
 class Document:
 	"""docstring for Document"""
 	def __init__(self, filename):
@@ -12,5 +14,10 @@ class Document:
 		with open(filename, mode="r", encoding="utf-8") as file:
 			for lines in file:
 				for word in lines.split(" "):
-					dict_words[word] = dict_words.get(word, 0) + 1
+					word = word.lower()
+					words = re.findall(r'\w+', word, re.UNICODE)
+
+					for word_filtered in words:
+						if len(word) >= 3:
+							dict_words[word] = dict_words.get(word, 0) + 1
 		return dict_words
